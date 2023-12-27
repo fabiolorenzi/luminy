fedef84e6c18c5e7cf9e8462eb9ff55659a82d7a
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-12-27 10:34:53 +0000

    developed basic enemy find player task

---
+++ Source/Luminy/EnemyFindPlayer_BTTask.cpp
@@ -0,0 +1,20 @@
+#include "EnemyFindPlayer_BTTask.h"
+#include "Kismet/GameplayStatics.h"
+#include "AIModule/Classes/BehaviorTree/BlackboardComponent.h"
+
+UEnemyFindPlayer_BTTask::UEnemyFindPlayer_BTTask()
+{
+    NodeName = "Find Player";
+}
+
+EBTNodeResult::Type UEnemyFindPlayer_BTTask::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    if (APawn* Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0)) {
+        auto Blackboard = OwnerComp.GetBlackboardComponent();
+        Blackboard->SetValueAsObject(PlayerKey.SelectedKeyName, Player);
+
+        return EBTNodeResult::Succeeded;
+    };
+
+    return EBTNodeResult::Failed;
+}
---
+++ Source/Luminy/EnemyFindPlayer_BTTask.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "EnemyFindPlayer_BTTask.generated.h"
+
+UCLASS()
+class LUMINY_API UEnemyFindPlayer_BTTask : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public:
+	UEnemyFindPlayer_BTTask();
+
+	UPROPERTY(EditAnywhere)
+		FBlackboardKeySelector PlayerKey;
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
